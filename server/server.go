1b30c487fa61b891408930e497d285e70b842d28
HermesKeng <kaining1101@gmail.com>
2020-12-20 00:46:55 -0800

    [Tritton Storage Back-end] Add token-based authentication for server

--- server/server.go
+++ server/server.go
@@ -6,11 +6,18 @@ import(
 	"time"
 	"io/ioutil"
 	"encoding/json"
+	"fmt"
 	rice "github.com/GeertJohan/go.rice"
 	mongo "go.mongodb.org/mongo-driver/mongo"
+	"auth"
 	"mydb"
 )
 
+type JsonBody struct{
+	Username string
+	Token string
+	IsSuccess bool
+}
 
 
 func main(){
@@ -43,11 +50,6 @@ func main(){
 
 func registerNewUser(app *rice.Box, c *mongo.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		indexFile, err := app.Open("index.html")
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
 		reqStr, err := ioutil.ReadAll(r.Body)
 		if err != nil{
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -72,21 +74,29 @@ func registerNewUser(app *rice.Box, c *mongo.Client) http.HandlerFunc {
 					if err != nil{
 						log.Println(err)
 					}
+					_, tokenString, err := auth.CreateToken(u.Username, u.Password, u.Email)
+					log.Println("new token: " + tokenString)
+					if err > 0{
+						log.Println("Internal Server Error")
+					}
+
+					body := JsonBody{Token: tokenString, Username: u.Username, IsSuccess: true}
+					json.NewEncoder(w).Encode(body)
 				}else{
 					log.Println("Users has already existed")
+					fmt.Fprintf(w, "false")
 				}
 		}
-		http.ServeContent(w, r, "index.html", time.Time{}, indexFile)
 	}
 }
+/*	
+http.SetCookie(w, &http.Cookie{Name:"token",Value: tokenString, Expires: expireTime,})
+*/
 func serveUsers(app *rice.Box, c *mongo.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		indexFile, err := app.Open("index.html")
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
+
 		reqStr, err := ioutil.ReadAll(r.Body)
+		log.Println(r.Cookie("token"))
 		if err != nil{
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
@@ -100,21 +110,32 @@ func serveUsers(app *rice.Box, c *mongo.Client) http.HandlerFunc {
 			log.Println(err)
 		}
 		collection := c.Database("tritonstorage").Collection("users")
-
+		w.Header().Set("content-type", "application/json")
 		switch r.Method {
 			case "POST":
-				log.Println("check")
 				isExist, user, err := mydb.GetUser(u.Email, collection)
 				
 				if err != nil{
 					log.Println(err)
 				}else if isExist && user.Password == u.Password{
 					log.Println("The user is verified by server")
-				}else if !isExist {
-					log.Println("data is not exist")
+					expireTime, tokenString, err := auth.CreateToken(u.Username, u.Password, u.Email)
+					if err > 0{
+						log.Println("Internal Server Error")
+					}
+					http.SetCookie(w, &http.Cookie{
+						Name:"token",
+						Value: tokenString,
+						Expires: expireTime,
+					})
+					body := JsonBody{Token: tokenString, Username: user.Username, IsSuccess: true}
+					json.NewEncoder(w).Encode(body)
+				}else {
+					log.Println("Account is incorrect or password incorrect")
+					
+					fmt.Fprintf(w, "false")
 				}
 		}
-		http.ServeContent(w, r, "index.html", time.Time{}, indexFile)
 	}
 }
 
