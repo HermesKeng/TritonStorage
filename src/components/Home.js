d1205a322fa8952a1ced4bd6a17dfc40ed44b2f4
HermesKeng <kaining1101@gmail.com>
2021-01-03 23:35:33 -0800

    [Triton Storage Front-end] Add Loading Icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import FileInfo from "./FileInfo.js";
 import { Link } from "react-router-dom";
 import { GoogleLogout } from "react-google-login";
+import LoadingDotIcon from "./LoadingDotIcon";
 import Axios from "axios";
 const fakeFile = [
   { id: 1, filename: "hello.txt", type: "document", version: 1 },
@@ -16,6 +17,7 @@ function Home(props) {
     localStorage.removeItem("tritonStorageUsername");
   }
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     async function fetchPostData() {
       try {
@@ -28,61 +30,70 @@ function Home(props) {
           }
         );
         setPosts(response.data);
+        setIsLoading(false);
       } catch (error) {
         console.log("error or the request is cancelled");
       }
     }
     fetchPostData();
   }, []);
-  return (
-    <>
-      <div className="row first">
-        <div className="col-4">
-          <h1> Hello! {props.name}</h1>
+  if (isLoading) {
+    return <LoadingDotIcon />;
+  } else {
+    return (
+      <>
+        <div className="row first">
+          <div className="col-4">
+            <h1> Hello! {props.name}</h1>
+          </div>
+          <div className="col-4"></div>
+          <div className="col-4">
+            <Link
+              to="/newfile"
+              type="button"
+              className="btn btn-outline-triton"
+            >
+              Upload File
+            </Link>
+            <button
+              type="button"
+              onClick={LogOut}
+              className="btn btn-outline-triton"
+            >
+              Log Out
+            </button>
+          </div>
         </div>
-        <div className="col-4"></div>
-        <div className="col-4">
-          <Link to="/newfile" type="button" className="btn btn-outline-triton">
-            Upload File
-          </Link>
-          <button
-            type="button"
-            onClick={LogOut}
-            className="btn btn-outline-triton"
-          >
-            Log Out
-          </button>
+        <div className="row">
+          <div className="col-1"></div>
+          <div className="col">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">Filename</th>
+                  <th scope="col">Type</th>
+                  <th scope="col">Version</th>
+                  <th scope="col">Download</th>
+                </tr>
+              </thead>
+              {posts.map((file, index) => (
+                <FileInfo
+                  id={file.Id}
+                  idx={index}
+                  key={file.Id}
+                  filename={file.Filename}
+                  type={file.Type}
+                  version={1}
+                />
+              ))}
+            </table>
+          </div>
+          <div className="col-1"></div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-1"></div>
-        <div className="col">
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Filename</th>
-                <th scope="col">Type</th>
-                <th scope="col">Version</th>
-                <th scope="col">Download</th>
-              </tr>
-            </thead>
-            {posts.map((file, index) => (
-              <FileInfo
-                id={file.Id}
-                idx={index}
-                key={file.Id}
-                filename={file.Filename}
-                type={file.Type}
-                version={1}
-              />
-            ))}
-          </table>
-        </div>
-        <div className="col-1"></div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default Home;
